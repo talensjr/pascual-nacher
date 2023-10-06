fa48cdae43bffa1931872a36bf10ca7716c64603
Jordi Talens <talensjr@gmail.com>
2023-10-06 16:40:01 +0200

    improve layout and scripts. add lang selector some styles

--- apps/dracs/astro.config.mjs
+++ apps/dracs/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), partytown()],
 });
