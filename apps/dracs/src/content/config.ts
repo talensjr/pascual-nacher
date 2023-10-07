5eef2b4c1a9933d427a5f17d3948d2b9daa1b661
Jordi Talens <talensjr@gmail.com>
2023-10-07 16:22:33 +0200

    added some styles

--- apps/dracs/src/content/config.ts
+++ apps/dracs/src/content/config.ts
@@ -1,7 +1,12 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
+  schema: z.object({
+    title: z.string(),
+    publishDate: z.date(),
+    lang: z.string(),
+  }),
 });
 
 export const collections = {
