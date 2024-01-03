b1f05ecfc11d0e231840600b2f563a8d16abe6d8
Jordi Talens <talensjr@gmail.com>
2024-01-03 11:37:03 +0100

    remove netlify identity

--- apps/dracs/src/env.d.ts
+++ apps/dracs/src/env.d.ts
@@ -1,7 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-// add netlifyIdentity to the global namespace
-interface Window {
-  netlifyIdentity: any;
-}
