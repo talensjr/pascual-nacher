1642464a4f3b2ad23054ea14ed9652ccea0efd6d
Jordi Talens <talensjr@gmail.com>
2024-01-03 08:51:06 +0100

    added posthog config and optimize scripts

--- apps/dracs/astro.config.mjs
+++ apps/dracs/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), partytown()],
+  integrations: [tailwind(), mdx()],
 });
